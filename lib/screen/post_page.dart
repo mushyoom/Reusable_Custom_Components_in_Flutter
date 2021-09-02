b4e535e8adc91dff58984b8721034e159590b6c3
mushyoom <mushyoom@gmail.com>
2021-09-03 01:57:31 +0900

    dropDown created

--- lib/screen/post_page.dart
+++ lib/screen/post_page.dart
@@ -9,6 +9,7 @@ class PostPage extends StatefulWidget {
 
 class _PostPageState extends State<PostPage> {
   bool _checkValue = false;
+  int _selectValue = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +24,12 @@ class _PostPageState extends State<PostPage> {
         height: 200,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [SizedBox(height: 20), _checkBox()],
+          children: [
+            SizedBox(height: 20),
+            _checkBox(),
+            SizedBox(height: 20),
+            _dropDown(),
+          ],
         ),
       ),
     );
@@ -53,5 +59,73 @@ class _PostPageState extends State<PostPage> {
             ),
           ),
         ));
+  }
+
+  //ドロップダウンボタン
+  Widget _dropDown() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(4),
+            boxShadow: [
+              BoxShadow(
+                color: Color(0xFFF2F2F2),
+                spreadRadius: 5,
+                blurRadius: 1,
+                offset: Offset(1, 1), // changes position of shadow
+              ),
+            ]),
+        child: InputDecorator(
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(borderSide: BorderSide.none),
+          ),
+          child: DropdownButtonHideUnderline(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 3),
+              child: DropdownButton(
+                  value: _selectValue,
+                  elevation: 2,
+                  itemHeight: 50,
+                  icon: Icon(Icons.arrow_circle_down),
+                  isDense: true,
+                  onChanged: (int? value) {
+                    print(value);
+                    setState(() {
+                      _selectValue = value!;
+                    });
+                  },
+                  items: [
+                    DropdownMenuItem(
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 18.0),
+                        child: Text(
+                          'A',
+                          style: TextStyle(fontSize: 14.0),
+                        ),
+                      ),
+                      value: 1,
+                    ),
+                    DropdownMenuItem(
+                      child: Text(
+                        'B',
+                        style: TextStyle(fontSize: 14.0),
+                      ),
+                      value: 2,
+                    ),
+                    DropdownMenuItem(
+                      child: Text(
+                        'C',
+                        style: TextStyle(fontSize: 14.0),
+                      ),
+                      value: 3,
+                    ),
+                  ]),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
